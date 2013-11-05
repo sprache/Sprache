618776f2d3af598b2cfd724a86131884c823f36a
IanWold <ilwold@hotmail.com>
2013-11-04 19:50:06 -0600

    Change Variable Names in ParserOfT
    Changed variable names in methods accepting TextReader.

--- src/Sprache/ParserOfT.cs
+++ src/Sprache/ParserOfT.cs
@@ -31,14 +31,21 @@ namespace Sprache
             return parser(new Input(input));
         }
 
-        public static IResult<T> TryParse<T>(this Parser<T> parser, TextReader _input)
+        /// <summary>
+        /// Tries to parse the input without throwing an exception.
+        /// </summary>
+        /// <typeparam name="T">The type of the result.</typeparam>
+        /// <param name="parser">The parser.</param>
+        /// <param name="input">The input.</param>
+        /// <returns>The result of the parser</returns>
+        public static IResult<T> TryParse<T>(this Parser<T> parser, TextReader input)
         {
-            string input = _input.ReadToEnd();
+            string _input = input.ReadToEnd();
 
             if (parser == null) throw new ArgumentNullException("parser");
             if (input == null) throw new ArgumentNullException("input");
 
-            return parser(new Input(input));
+            return parser(new Input(_input));
         }
 
         /// <summary>
@@ -62,14 +69,22 @@ namespace Sprache
             throw new ParseException(result.ToString());
         }
 
-        public static T Parse<T>(this Parser<T> parser, TextReader _input)
+        /// <summary>
+        /// Parses the specified input string.
+        /// </summary>
+        /// <typeparam name="T">The type of the result.</typeparam>
+        /// <param name="parser">The parser.</param>
+        /// <param name="input">The input.</param>
+        /// <returns>The result of the parser.</returns>
+        /// <exception cref="Sprache.ParseException">It contains the details of the parsing error.</exception>
+        public static T Parse<T>(this Parser<T> parser, TextReader input)
         {
-            string input = _input.ReadToEnd();
+            string _input = input.ReadToEnd();
 
             if (parser == null) throw new ArgumentNullException("parser");
-            if (input == null) throw new ArgumentNullException("input");
+            if (_input == null) throw new ArgumentNullException("input");
 
-            var result = parser.TryParse(input);
+            var result = parser.TryParse(_input);
 
             if (result.WasSuccessful)
                 return result.Value;
