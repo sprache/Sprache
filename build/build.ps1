ab429d688ff173e4888c136ba852652f0c9762a8
Nicholas Blumhardt <nblumhardt@nblumhardt.com>
2016-08-09 20:05:45 +1000

    Switch to --version-suffix based versioning and publish from develop branch

--- build/build.ps1
+++ build/build.ps1
@@ -1,25 +1,11 @@
-param (
-	[string]$BuildVersionNumber=$(throw "-BuildVersionNumber is required."),
-	[string]$TagVersionNumber
-)
+$branch = @{ $true = $env:APPVEYOR_REPO_BRANCH; $false = $(git symbolic-ref --short -q HEAD) }[$env:APPVEYOR_REPO_BRANCH -ne $NULL];
+$revision = @{ $true = "{0:00000}" -f [convert]::ToInt32("0" + $env:APPVEYOR_BUILD_NUMBER, 10); $false = "local" }[$env:APPVEYOR_BUILD_NUMBER -ne $NULL];
+$suffix = @{ $true = ""; $false = "$($branch.Substring(0, [math]::Min(10,$branch.Length)))-$revision"}[$branch -eq "master" -and $revision -ne "local"]
 
-# set version number in package.json
-Get-ChildItem -Path $PSScriptRoot\..\src -Filter project.json -Recurse | ForEach-Object{ 
-    $ProjectJsonPath =  $_.FullName
-    if ($TagVersionNumber){
-        (gc -Path $ProjectJsonPath) `
-	        -replace "(?<=`"version`":\s`")[.\w-]*(?=`",)", "$TagVersionNumber" |
-	            sc -Path $ProjectJsonPath -Encoding UTF8
-    }
-    else{
-        (gc -Path $ProjectJsonPath) `
-	        -replace "(?<=`"version`":\s`")[.\w-]*(?=`",)", "$BuildVersionNumber" |
-	            sc -Path $ProjectJsonPath -Encoding UTF8
-    }
-}
+echo "build: Version suffix is $suffix"
 
 # run restore on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
-Get-ChildItem -Path $PSScriptRoot\..\src -Filter project.json -Recurse | ForEach-Object { & dotnet restore $_.FullName 2>1 }
+Get-ChildItem -Path $PSScriptRoot\..\src -Filter project.json -Recurse | ForEach-Object { & dotnet restore $_.FullName --no-cache 2>1 }
 
 # run pack on all project.json files in the src folder including 2>1 to redirect stderr to stdout for badly behaved tools
-Get-ChildItem -Path $PSScriptRoot\..\src -Filter project.json -Recurse | ForEach-Object { & dotnet pack $_.FullName -c Release 2>1 }
+Get-ChildItem -Path $PSScriptRoot\..\src -Filter project.json -Recurse | ForEach-Object { & dotnet pack $_.FullName -c Release --version-suffix=$suffix 2>1 }
